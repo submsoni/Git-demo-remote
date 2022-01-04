fedd0caf0af290dbabebdaa6fe6ee1d8ba9bda05
submsoni <77857326+submsoni@users.noreply.github.com>
2022-01-04 13:28:26 +0530

    math.py added

---
+++ math.py
@@ -0,0 +1,13 @@
+# Add Implementation
+def add(x,y):
+    return x+y
+# Sub Implementation
+def subtract(x,y):
+    return x-y #commit on master branch
+# Mul Implementation
+def multiply(x,y):
+    return x*y #on bug456 branch 
+# Div Implementation
+def divide(x,y):
+    if(x == 0)
+	x*x  
